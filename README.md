9e4f2652908e1e936a4034f715393ed890c17f0b
Ar1k <arunkmr492@gmail.com>
2015-04-01 14:55:21 +0530

    v1-pre alpha

---
+++ README.md
@@ -0,0 +1,2 @@
+# youtube_remote
+Use your phone as a remote control for youtube
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var io = require('socket.io').listen(app.listen(3000));
+
+
+
+//requests coming for desktop
+app.get('/', function(req, res){
+   //console.log(req.query.id);
+   res.sendfile(__dirname + '/public/server.html');
+});
+
+
+//requests coming for mobile
+app.get('/mobile/:id', function(req, res){
+   //console.log(req.params.id);
+   res.sendfile(__dirname + '/public/index.html');
+});
+
+// handle rest of the requests
+app.use(express.static(__dirname + '/public'));
+
+
+//socket magic
+io.sockets.on('connection', function (socket) {
+
+	socket.on('new_room', function(data){
+		socket.join(data);		
+	});
+
+	socket.on('play/pause', function(data){
+		room = data.roomId;
+		socket.broadcast.to(room).emit('play/pause', data);
+	});
+});
