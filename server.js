fca2062cc53227ff5dde2056c2978f12998e9f53
Ar1k <arunkmr492@gmail.com>
2015-04-01 15:08:07 +0530

    file name changes

--- server.js
+++ server.js
@@ -7,14 +7,14 @@ var io = require('socket.io').listen(app.listen(3000));
 //requests coming for desktop
 app.get('/', function(req, res){
    //console.log(req.query.id);
-   res.sendfile(__dirname + '/public/server.html');
+   res.sendfile(__dirname + '/public/index.html');
 });
 
 
 //requests coming for mobile
 app.get('/mobile/:id', function(req, res){
    //console.log(req.params.id);
-   res.sendfile(__dirname + '/public/index.html');
+   res.sendfile(__dirname + '/public/mobile.html');
 });
 
 // handle rest of the requests
